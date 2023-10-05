f374d792b8e88d6c8be24022589d9f4c2ff41ca3
Menaid <134172054+Menaid@users.noreply.github.com>
2023-10-05 15:41:16 +0200

    added ReadAllLines and for loops

--- ForAdmin.cs
+++ ForAdmin.cs
@@ -1,6 +1,41 @@
 ﻿namespace WebShop4;
 
-internal class ForAdmin : Customers
+public class ForAdmin
 {
+    bool editCustomerInfo = true;
+    bool editeProductList = true;
+    bool Oversight = true;
+
+    // string[] products = File.ReadAllLines(".....txt");
+    // string[] oversight = File.ReadAllLines("....txt");
+    public static void CustomerInfo()
+    {
+        string[] customers = File.ReadAllLines("../../../customers.txt");
+
+        //for (int i = 0; i < customers.Length; i++)
+        //{
+        //    var viewCustomers = customers[i];
+        //    Console.WriteLine((i + 1) + ". " + viewCustomers);
+        //}
+
+        for (int i = 0; i < customers.Length; i++)
+        {
+            Console.WriteLine(customers[i]);
+            File.WriteAllLines("../../../customers.txt", customers);
+        }
+
+        return;
+
+    }
+
+    //public string ProductList()
+    //{
+
+    //}
+
+    //public string Oversight()
+    //{
+
+    //}
 
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using WebShop4;
+
 Console.WriteLine("Hello, World!");
+ForAdmin.CustomerInfo();
