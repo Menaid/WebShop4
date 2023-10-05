7332a4a799e59ca8746a840b3f1a8d0296d23f46
Fungiboy <Lukas.Austrell@gmail.com>
2023-10-06 01:27:30 +0200

    Har fixat en login metod som funkar med txt filen
    Är färdig med registrering och login av kunder

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ string A = Console.ReadLine();
 switch (A.ToString().ToLower())
 {
     case "l":
-        Console.WriteLine("L");
+        LogIn();
         break;
     case "r":
         Register();
@@ -23,9 +23,31 @@ void Register()
 
     customer custom = new customer(username: Uname, password: Pword);
 
-    string UP = Uname +"-"+ Pword;
+    string UP = Uname + "-" + Pword;
 
     string loca = @"../../../customers.txt";
     File.AppendAllText(loca, UP + Environment.NewLine);
 }
 
+void LogIn()
+{
+    Console.WriteLine("Skriv användarnamn: ");
+    string Usname = Console.ReadLine();
+    Console.WriteLine("Skriv Lösenord: ");
+    string Paword = Console.ReadLine();
+    string[] lines = File.ReadAllLines(@"../../../customers.txt");
+    List<string> newwords = new List<string>();
+    foreach (var item in lines)
+    {
+        newwords = new List<string>(item.Split("-"));
+        if (Usname == newwords[0] && Paword == newwords[1])
+        {
+            Console.WriteLine("Correct");
+            break;
+        }
+    }
+    if (Usname != newwords[0] || Paword != newwords[1])
+    {
+        Console.WriteLine("Not Correct");
+    }
+}
