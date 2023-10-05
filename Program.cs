965d3d22e64a60c87db67ca5889f2f9f4ae20afe
Fungiboy <Lukas.Austrell@gmail.com>
2023-10-05 16:49:16 +0200

    Fixat registreringen av nya kunder appendat deras användar namn och lösenord till txt filen.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,31 @@
-﻿Console.WriteLine("Do you want to register or log in?:");
+﻿using WebShop4;
+
+Console.WriteLine("Do you want to register or log in?:");
 Console.Write("| L = Login | R = Register | ");
 Console.WriteLine(" ");
 string A = Console.ReadLine();
 
-switch(A.ToString().ToLower())
+switch (A.ToString().ToLower())
 {
     case "l":
         Console.WriteLine("L");
         break;
     case "r":
-        Console.WriteLine("R");
+        Register();
         break;
-}   
+}
+void Register()
+{
+    Console.WriteLine("Skriv användarnamn: ");
+    string Uname = Console.ReadLine();
+    Console.WriteLine("Skriv Lösenord: ");
+    string Pword = Console.ReadLine();
+
+    customer custom = new customer(username: Uname, password: Pword);
+
+    string UP = Uname +"-"+ Pword;
+
+    string loca = @"../../../customers.txt";
+    File.AppendAllText(loca, UP + Environment.NewLine);
+}
+
