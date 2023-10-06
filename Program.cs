a29ea965baa33913cfaa3f8ee8f67f5b5f6bc983
Jimmy Hansen <40581118+Ymmuu@users.noreply.github.com>
2023-10-06 14:29:49 +0200

    skickat över rätt info

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 using WebShop4;
 
+ForAdmin.CustomerInfo();
 
-<<<<<<< HEAD
-Console.WriteLine("Test");
-=======
-//register.startCustom();
->>>>>>> 286877a23986b0b0efdb2dce67d91652e8aa4338
+register.startCustom();
