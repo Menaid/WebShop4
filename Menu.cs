1f5bc60448a30232ddf9a52e8f3e8386d4ff4cfc
Menaid <134172054+Menaid@users.noreply.github.com>
2023-10-10 14:11:19 +0200

    Added ShoppingCart Class

--- Menu.cs
+++ Menu.cs
@@ -66,13 +66,7 @@
                         run = false;
                         break;
                     case "3":
-
-                        Console.WriteLine("Vill du redigera kundinfo?");
-                        string input = Console.ReadLine().ToLower();
-                        if (input == "ja")
-                        {
-                            ForAdmin.CustomerInfo();
-                        }
+                        ForAdmin.CustomerInfo();
                         run = false;
                         break;
                     case "4":
@@ -93,9 +87,30 @@
 
         }
 
-        public static void UserChoice()
-        {
 
+        public static void UserMenu()
+        {
+            Console.WriteLine("Menu:");
+            Console.Write("Varukorg = V\nProduktlista = P\nGenomförda beställningar och se kvitton = G\nExit = E");
+            Console.WriteLine(" ");
+            string A = Console.ReadLine();
+
+            switch (A.ToString().ToLower())
+            {
+                case "v":
+                    Console.WriteLine("Varukorg");
+                    break;
+                case "p":
+                    Console.WriteLine("Produktlista");
+                    break;
+                case "g":
+                    Console.WriteLine("Genomförda beställningar och kvitton");
+                    break;
+                case "e":
+                    UserMenu();
+                    break;
+            }
         }
+
     }
 }
