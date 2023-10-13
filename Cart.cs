e52a52aa4b8c933fe1ee08b1ae1f20544e5ec422
Menaid <134172054+Menaid@users.noreply.github.com>
2023-10-13 13:42:54 +0200

    adjustments

--- Cart.cs
+++ Cart.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace WebShop4;
 
 public class Cart
@@ -11,12 +6,12 @@ public class Cart
     public List<Products> Items;
     public Cart(string username)
     {
-        string[] data = File.ReadAllLines("../../../carts/"+username+".csv");
+        string[] data = File.ReadAllLines("../../../carts/" + username + ".csv");
 
-        foreach(string item in data)
+        foreach (string item in data)
         {
             Items.Add(new Products(item));
         }
-        
+
     }
 }
--- Products.cs
+++ Products.cs
@@ -11,10 +11,9 @@ namespace WebShop4
         public string? ProductName;
         public int Quantity;
         public int Price;
-        
+
         public static void ProductList()
         {
-            
             string[] ItemsInCollection = File.ReadAllLines("../../../produktlista.txt");
             Console.Clear();
             Console.WriteLine("----------");
@@ -22,19 +21,13 @@ namespace WebShop4
             {
                 Console.WriteLine((i + 1) + ". " + ItemsInCollection[i]);
             }
-<<<<<<< HEAD
-            Console.WriteLine("Vill du lägga till någon produkt till din varukorg? \n eller vill du gå tillbaka till menyn? \n Ja för att lägga till | Nej för att gå tillbaka till menyn");
-=======
             Console.WriteLine("----------");
             Console.WriteLine("Vill du lägga till någon produkt till din varukorg? \n eller vill du gå tillbaka till menyn? \n J för att lägga till | N för att gå tillbaka till menyn");
->>>>>>> 7ec2876f6fcbab07e102c3dfdce3fae194e6dfcd
+
             string? DoOrDoNot = Console.ReadLine().ToLower();
 
             if (DoOrDoNot == "j")
             {
-<<<<<<< HEAD
-                ForAdmin.addItem();
-=======
                 Console.Clear();
                 Console.WriteLine("----------");
                 for (int i = 0; i < ItemsInCollection.Length; i++)
@@ -43,7 +36,7 @@ namespace WebShop4
                 }
                 Console.WriteLine("----------");
                 Console.WriteLine("Vilken produkt vill du lägga till?: ");
-                int input = int.Parse(Console.ReadLine())-1;
+                int input = int.Parse(Console.ReadLine()) - 1;
                 string a = input.ToString();
 
                 for (int i = 0; i < ItemsInCollection.Length; i++)
@@ -53,11 +46,8 @@ namespace WebShop4
                         string username = "username";
                         string loca = "../../../carts/" + username + ".csv";
                         File.AppendAllText(loca, ItemsInCollection[i] + Environment.NewLine);
-
                     }
-                    
                 }
->>>>>>> 7ec2876f6fcbab07e102c3dfdce3fae194e6dfcd
             }
             else
             {
