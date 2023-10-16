70ed9208c57d16a77657739350e738aaf1e2ccc9
Jimmy Hansen <40581118+Ymmuu@users.noreply.github.com>
2023-10-16 12:07:22 +0200

    en detalj kvar

--- signup.cs
+++ signup.cs
@@ -1,38 +1,60 @@
 ﻿namespace WebShop4;
-
-public static void SignUp()
+public class Customer
 {
-    
-Console.WriteLine("Registrera dig som ny kund");
-Console.WriteLine("--------------------------");
-Console.WriteLine("Ange ditt namn: ");
-string name = Console.ReadLine();
-Console.Clear();
+    public string UserName { get; set; }
+    public string UserPw { get; set; }
+    public int UserId { get; set; }
 
-string[] customer = File.ReadAllLines("../../../customer.txt");
-if (customer.Contains(name))
-{
-    Console.WriteLine("Det finns redan en användare med namnet " + name +  " vänligen välj ett annat.");
-    return;
-}
+    public Customer(string userName, string userPw, int userId)
+    {
+        UserName = userName;
+        UserPw = userPw;
+        UserId = userId;
+    }
 
-Console.WriteLine("Ange önskat lösenord: ");
-string pw = Console.ReadLine();
-Console.WriteLine("Bekräfta lösenordet: ");
-string pw1 = Console.ReadLine();
+    public static void SignUp()
+    {
 
-while (pw != pw1)
-{
-    Console.WriteLine("Du angav inte samma lösenord, försök igen");
-}
-if (pw == pw1)
-{
-    Console.WriteLine("Grattis, du är nu en registrerad kund!");
-}
+        Console.WriteLine("Registrera dig som ny kund");
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("Ange ditt namn: ");
+        string name = Console.ReadLine();
+        Console.Clear();
 
-string newCustomer = name + "-" + pw;
-File.AppendAllText(customer[], newCustomer, Environment.NewLine);
+        string[] customer = File.ReadAllLines("../../../customer.txt");
+        if (customer.Contains(name))
+        {
+            Console.WriteLine("Det finns redan en användare med namnet " + name + " vänligen välj ett annat.");
+            return;
+        }
 
+        Console.WriteLine("Ange önskat lösenord: ");
+        string pw = Console.ReadLine();
+        Console.WriteLine("Bekräfta lösenordet: ");
+        string pw1 = Console.ReadLine();
+
+        while (pw != pw1 || pw == null)
+        {
+            Console.WriteLine("Du angav inte samma lösenord eller så lämnade du fältet tomt, försök igen");
+        }
+        if (pw == pw1)
+        {
+            Console.WriteLine("Grattis, du är nu en registrerad kund!");
+        }
+
+        Customer Costumer = new Customer(userName: name, userPw: pw, userId: GenerateUniqueId());
+
+
+
+        string newCustomer = name + "-" + pw;
+        File.AppendAllText(customer[], newCustomer, Environment.NewLine);
+
+
+        static int GenerateUniqueId()
+        {
+            return Guid.NewGuid().GetHashCode();
+        }
+    }
 }
 
 
