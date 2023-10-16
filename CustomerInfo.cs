f0fd2305f806e3ee49838ad50971e8567c0c41ce
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2023-10-16 10:09:54 +0200

    skrivit klart show customer info

---
+++ CustomerInfo.cs
@@ -0,0 +1,32 @@
+﻿
+using System.ComponentModel;
+using WebShop4;
+namespace WebShop4;
+
+public class CustomerInfo
+{
+    
+
+
+
+    public static void ShowInfo()
+    {
+        string[] file = File.ReadAllLines("../../../customers.txt");
+        List<string> CustomerList = new List<string>();
+
+        foreach (string item in file)
+        {
+            CustomerList.Add(item);
+            //CustomerList = new List<string>(item.Split("-"));
+        }
+        foreach(string item in CustomerList)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public void EditInfo()
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using WebShop4;
+
 Console.WriteLine("Hello, World!");
+
+CustomerInfo.ShowInfo();
