8552e51a77f6e01bce7b20439a2cba242be776d8
Jimmy Hansen <40581118+Ymmuu@users.noreply.github.com>
2023-10-16 10:45:52 +0200

    added check on password

---
+++ signup.cs
@@ -0,0 +1,26 @@
+﻿namespace WebShop4;
+
+public static void SignUp()
+{
+    Console.WriteLine("Registrera dig som ny kund");
+    Console.WriteLine("--------------------------");
+    Console.WriteLine("Ange ditt namn: ");
+    string name = Console.ReadLine();
+    Console.Clear();
+
+    Console.WriteLine("Ange önskat lösenord: ");
+    string pw = Console.ReadLine();
+    Console.WriteLine("Bekräfta lösenordet: ");
+    string pw1 = Console.ReadLine();
+
+    while (pw != pw1)
+    {
+        Console.WriteLine("Du angav inte samma lösenord, försök igen");
+    }
+    if (pw == pw1)
+    {
+        Console.WriteLine("Grattis, du är nu en registrerad kund!");
+    }
+
+
+}
