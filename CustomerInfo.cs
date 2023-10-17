c0641a58554be0b31a3b0bd1872c19215069b458
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2023-10-17 10:35:06 +0200

    la till kommmentarer

--- CustomerInfo.cs
+++ CustomerInfo.cs
@@ -5,60 +5,62 @@ namespace WebShop4;
 
 public class CustomerInfo
 {
-    
-
     public static void EditInfo()
     {
-        string[] file = File.ReadAllLines("../../../customers.txt");
-        List<string> CustomerList = new List<string>();
+        string[] file = File.ReadAllLines("../../../customers.txt"); // läser användare från fil
+        List<string> CustomerList = new List<string>(); //skapar ny lista
 
-        foreach (string item in file)
+        foreach (string item in file)//kopierar användare från fil till lista
         {
             CustomerList.Add(item);
-            CustomerList = new List<string>(item.Split("-"));
-
         }
 
-        for (int i = 0; i < file.Length; i++)
+        for (int i = 0; i < CustomerList.Count; i++) //skriver ut lista på användare
         {
-            Console.WriteLine(i + 1 + ". " + file[i]);
-
+            Console.WriteLine(i + 1 + ". " + CustomerList[i]);
         }
 
-        Console.WriteLine("Vill du (1)ändra på en användare eller (2)gå tillbaka?");
+        Console.WriteLine("Vill du (1)ändra på en användare eller (2)gå tillbaka?");//ger val för edit eller exit
         var choice = Console.ReadLine();
 
         if (choice == "1")
         {
             Console.WriteLine("Välj siffran för den användare som ska redigeras: ");
             string number = Console.ReadLine();
-            int userNumber = int.Parse(number) - 1;
+            int userNumber = int.Parse(number) - 1;// svaret -1 pga lista börjar på 0
 
-            for (int i = 0; i < CustomerList.Count; i++)
+            for (int i = 0; i < CustomerList.Count; i++) //loop för att se ifall index = input
             {
-                if (i == userNumber)
+                if (CustomerList[userNumber] == CustomerList[i])
                 {
+                    string[] line = CustomerList[i].Split('-');// gör en array av namnet och lösen för att split på "-"
+                    foreach(string item in line)
+                    {
+                        Console.WriteLine(item);
+                    }
                     Console.WriteLine("Vill du ändra (1)användarnamn eller (2)lösenord?");
-                    string nameOrPass = Console.ReadLine();
-                    switch (nameOrPass)
+                    string nameOrPassword = Console.ReadLine();
+
+                    var a = file[userNumber];
+                    CustomerList = new List<string>(a.Split("-"));
+
+                    switch (nameOrPassword)
                     {
                         case "1":
                             Console.WriteLine("Nytt namn: ");
                             var NewName = Console.ReadLine();
-                            CustomerList[0] = NewName;
+                            line[0] = NewName;
 
                             break;
                         case "2":
                             Console.WriteLine("Nytt lösenord: ");
                             var NewPassword = Console.ReadLine();
-                            CustomerList[1] = NewPassword;
+                            line[1] = NewPassword;
                             break;
                     }
 
-                    int b = int.Parse(nameOrPass);
-                    b -= 1;
-                    file[b] = CustomerList[0] + "-" + CustomerList[1];
-                    File.WriteAllLines("../../../customers.txt", file);
+                    line[i] = line[0] + "-" + line[1];
+                    File.WriteAllLines("../../../customers.txt", line);
                     Console.Clear();
                 }
                 else
@@ -74,7 +76,7 @@ public class CustomerInfo
             Console.WriteLine("exit");
         }
 
-        
+
 
         var input = Console.ReadLine();
         if (input == "")
@@ -82,8 +84,8 @@ public class CustomerInfo
             Console.Clear();
             //Menu.AdminChoice();
         }
-        
-        
+
+
 
 
     }
