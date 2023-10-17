9c659de60dc7aa1d20d26007f05cc340fe052586
Fungiboy <Lukas.Austrell@gmail.com>
2023-10-17 11:43:30 +0200

    Kortade ner koden

--- CustomerInfo.cs
+++ CustomerInfo.cs
@@ -10,38 +10,29 @@ public class CustomerInfo
     {
         string[] file = File.ReadAllLines("../../../customers.txt"); // läser användare från fil
         List<string> CustomerList = new List<string>(); //skapar ny lista
-
-        foreach (string item in file)//kopierar användare från fil till lista
+        for (int i = 0; i < file.Length; i++) //skriver ut lista på användare
         {
-            CustomerList.Add(item);
-        }
-
-        for (int i = 0; i < CustomerList.Count; i++) //skriver ut lista på användare
-        {
+            CustomerList.Add(file[i]);
             Console.WriteLine(i + 1 + ". " + CustomerList[i]);
         }
-
         Console.WriteLine("Vill du (1)ändra på en användare eller (2)gå tillbaka?");//ger val för edit eller exit
         var choice = Console.ReadLine();
 
         if (choice == "1")
         {
             Console.WriteLine("Välj siffran för den användare som ska redigeras: ");
-            string number = Console.ReadLine();
-            int userNumber = int.Parse(number) - 1;// svaret -1 pga lista börjar på 0
-
+            int number = int.Parse(Console.ReadLine()) - 1;
             for (int i = 0; i < file.Length; i++) //loop för att se ifall index = input
             {
-                if (userNumber == i)
+                if (number == i)
                 {
-                    var thing = CustomerList[userNumber].Split('-');// gör en array av namnet och lösen för att split på "-"
+                    var thing = CustomerList[number].Split('-');// gör en array av namnet och lösen för att split på "-"
                     foreach(string item in thing)
                     {
                         Console.WriteLine(item);
                     }
                     Console.WriteLine("Vill du ändra (1)användarnamn eller (2)lösenord?");
                     string nameOrPassword = Console.ReadLine();
-
                     switch (nameOrPassword)
                     {
                         case "1":
@@ -56,18 +47,15 @@ public class CustomerInfo
                             thing[1] = NewPassword;
                             break;
                     }
-
-                    file[userNumber] = thing[0] + "-" + thing[1];
+                    file[number] = thing[0] + "-" + thing[1];
                     File.WriteAllLines("../../../customers.txt", file);
                     Console.Clear();
                 }
             }
         }
-
         else
         {
             Console.WriteLine("exit");
         }
-
     }
 }
