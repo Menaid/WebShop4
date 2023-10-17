69a0548f6eabdb25fbaeef8c448547d7ba7626b7
Fungiboy <Lukas.Austrell@gmail.com>
2023-10-17 11:24:28 +0200

    Fixade fel

--- CustomerInfo.cs
+++ CustomerInfo.cs
@@ -1,12 +1,11 @@
 ﻿
 using System.ComponentModel;
+using System.Reflection.Metadata;
 using WebShop4;
 namespace WebShop4;
 
 public class CustomerInfo
 {
-    
-
     public static void EditInfo()
     {
         string[] file = File.ReadAllLines("../../../customers.txt");
@@ -15,14 +14,11 @@ public class CustomerInfo
         foreach (string item in file)
         {
             CustomerList.Add(item);
-            CustomerList = new List<string>(item.Split("-"));
-
         }
 
         for (int i = 0; i < file.Length; i++)
         {
             Console.WriteLine(i + 1 + ". " + file[i]);
-
         }
 
         Console.WriteLine("Vill du (1)ändra på en användare eller (2)gå tillbaka?");
@@ -34,37 +30,31 @@ public class CustomerInfo
             string number = Console.ReadLine();
             int userNumber = int.Parse(number) - 1;
 
-            for (int i = 0; i < CustomerList.Count; i++)
-            {
-                if (i == userNumber)
+            for (int i = 0; i < file.Length; i++)
+            {   
+                if (userNumber == i)
                 {
+                    CustomerList = new List<string>(file[userNumber].Split("-"));
                     Console.WriteLine("Vill du ändra (1)användarnamn eller (2)lösenord?");
                     string nameOrPass = Console.ReadLine();
                     switch (nameOrPass)
                     {
                         case "1":
                             Console.WriteLine("Nytt namn: ");
-                            var NewName = Console.ReadLine();
+                            string NewName = Console.ReadLine();
                             CustomerList[0] = NewName;
 
                             break;
                         case "2":
                             Console.WriteLine("Nytt lösenord: ");
-                            var NewPassword = Console.ReadLine();
+                            string NewPassword = Console.ReadLine();
                             CustomerList[1] = NewPassword;
                             break;
                     }
 
-                    int b = int.Parse(nameOrPass);
-                    b -= 1;
-                    file[b] = CustomerList[0] + "-" + CustomerList[1];
+                    file[userNumber] = CustomerList[0] + "-" + CustomerList[1];
                     File.WriteAllLines("../../../customers.txt", file);
                     Console.Clear();
-                }
-                else
-                {
-                    Console.WriteLine("Kunde inte hitta siffran i listan.");
-                    EditInfo();
                 }
             }
         }
@@ -73,8 +63,6 @@ public class CustomerInfo
         {
             Console.WriteLine("exit");
         }
-
-        
 
         var input = Console.ReadLine();
         if (input == "")
