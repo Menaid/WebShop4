9862ec5278ea1423348a2061bcb1fbe8de19ed29
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2023-10-17 11:35:34 +0200

    initialise

--- Test.cs
+++ Test.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WebShop4;
+namespace WebShop4;
 
-namespace WebShop4
+internal class Test
 {
-    internal class Test
+    public static void Tests()
     {
+
     }
 }
