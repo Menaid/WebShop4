aab497c90463d6e77f56bea9e58ae223a7d0891d
Menaid <134172054+Menaid@users.noreply.github.com>
2023-10-18 21:34:37 +0200

    Using Rider for the first time

--- Transactions.cs
+++ Transactions.cs
@@ -1,13 +1,3 @@
 ﻿namespace WebShop4;
 
-public record Transactions(string userID, string product, float price, int quantity, DateTime transcationtime)
-{
-    public Transactions
-    {
-        UserID = userID;
-        Product = product;
-        Price = price;
-        Quantity = quantity;
-        Transactiontime = transcationtime;
-    }
-}
+public record Transactions(string userID, string product, float price, int quantity, DateTime transcationtime);
