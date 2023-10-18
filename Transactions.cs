ce96640dc8d32bdd76eaf1a8a6247147c5e50c34
Menaid <134172054+Menaid@users.noreply.github.com>
2023-10-18 20:00:08 +0200

    just added transaction record

---
+++ Transactions.cs
@@ -0,0 +1,13 @@
+﻿namespace WebShop4;
+
+public record Transactions(string userID, string product, float price, int quantity, DateTime transcationtime)
+{
+    public Transactions
+    {
+        UserID = userID;
+        Product = product;
+        Price = price;
+        Quantity = quantity;
+        Transactiontime = transcationtime;
+    }
+}
